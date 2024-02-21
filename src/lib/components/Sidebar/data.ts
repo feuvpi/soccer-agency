51bb175ea8a6ea0ddc62ebc464cba624bdc21b7b
Fred Pinheiro <fredvpg@gmail.com>
2024-02-20 21:49:53 -0300

    .

---
+++ src/lib/components/Sidebar/data.ts
@@ -0,0 +1,46 @@
+// @ts-nocheck
+import HomeIcon from './icons/HomeIcon.svelte';
+import BillsIcon from './icons/BillsIcon.svelte';
+import AnalyticsIcon from './icons/AnalyticsIcon.svelte';
+import MonitoringIcon from './icons/MonitoringIcon.svelte';
+import DemographicsIcon from './icons/DemographicsIcon.svelte';
+import ApplicationIcon from './icons/ApplicationIcon.svelte';
+import DocumentationIcon from './icons/DocumentationIcon.svelte';
+
+export const data = [
+	{
+		title: 'Home',
+		icon: HomeIcon,
+		link: '/'
+	},
+	{
+		title: 'Bills',
+		icon: BillsIcon,
+		link: '/admin/bills'
+	},
+	{
+		title: 'Applications',
+		icon: ApplicationIcon,
+		link: '/admin/applications'
+	},
+	{
+		title: 'Monitoring',
+		icon: MonitoringIcon,
+		link: '/admin/monitoring'
+	},
+	{
+		title: 'Demographics',
+		icon: DemographicsIcon,
+		link: '/admin/demographics'
+	},
+	{
+		title: 'Analytics',
+		icon: AnalyticsIcon,
+		link: '/admin/analytics'
+	},
+	{
+		title: 'Documentation',
+		icon: DocumentationIcon,
+		link: '/admin/documentation'
+	}
+];
--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -1,5 +1,38 @@
-<script lang="ts">
-	import type { LayoutData } from './$types';
+<script>
+	import 'tailwindcss/tailwind.css';
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
 
-	export let data: LayoutData;
+	import TopBar from '$lib/components/Topbar.svelte'
+	import Overlay from '$lib/components/Overlay.svelte';
+	import Sidebar from '$lib/components/Sidebar/Sidebar.svelte'
+	import { closeSidebar, sidebarOpen } from '../../lib/utils/store';
+
+	const style = {
+		container: `bg-gray-100 h-screen overflow-hidden relative`,
+		main: `h-screen overflow-auto pb-36 pt-4 px-2 md:pb-8 lg:px-4`,
+		mainContainer: `flex flex-col h-screen pl-0 w-full lg:pl-24 lg:space-y-4`
+	};
+
+	if (browser) {
+		page.subscribe(() => {
+			// close Sidebar on route changes when sidebar is open
+			if ($sidebarOpen) {
+				closeSidebar();
+			}
+		});
+	}
 </script>
+
+<div class={style.container} style="background-image: url('https://images.unsplash.com/photo-1561457909-cbbf7d732ee6?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-size: cover; background-color: rgba(255, 255, 255, 0.5);">
+	<div class="flex items-start">
+		<Overlay />
+		<Sidebar mobileOrientation="end" />
+		<div class={style.mainContainer}>
+			<TopBar />
+			<main class={style.main}>
+				<slot />
+			</main>
+		</div>
+	</div>
+</div>
